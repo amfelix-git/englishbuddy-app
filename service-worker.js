e4edc475efd5f59f387598f652ca9416f644f516
amfelix <amfelix@gmail.com>
2026-07-26 23:08:18 +0100

    Adiciona sessões 31-40 do Nível Intermédio
    Word families (prefixos/sufixos), collocations avançadas, phrasal
    verbs profissionais (deal with/come up with/point out), idiomas de
    negócios, funções comunicativas (persuadir/negociar), leitura de
    artigo de opinião, escrita de ensaio argumentativo, pronúncia (acento
    em frases), cultura (humor/ironia), e sessão integradora 'Reunião de
    Trabalho'.

--- service-worker.js
+++ service-worker.js
@@ -34,6 +34,7 @@ const FICHEIROS_PRECACHE = [
   './content/intermedio/sessoes-01-10.json',
   './content/intermedio/sessoes-11-20.json',
   './content/intermedio/sessoes-21-30.json',
+  './content/intermedio/sessoes-31-40.json',
   'https://unpkg.com/dexie@4.0.8/dist/dexie.min.js'
 ];
 
