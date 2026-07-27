fd2f3f87269110c26dd25189edd96e86ccb41d34
amfelix <amfelix@gmail.com>
2026-07-27 08:55:32 +0100

    Adiciona sessões 81-90 do Nível Intermédio (marco: 90/140)
    Wish + past simple/perfect, would rather, exame First Reading partes
    5-7, Writing article, vocabulário viagens avançado e tecnologia,
    phrasal verbs de viagem (set off/check in/turn up), Speaking partes 1
    e 3/4, e sessão de marco 'Viagem de Negócios'.

--- service-worker.js
+++ service-worker.js
@@ -39,6 +39,7 @@ const FICHEIROS_PRECACHE = [
   './content/intermedio/sessoes-51-60.json',
   './content/intermedio/sessoes-61-70.json',
   './content/intermedio/sessoes-71-80.json',
+  './content/intermedio/sessoes-81-90.json',
   'https://unpkg.com/dexie@4.0.8/dist/dexie.min.js'
 ];
 
