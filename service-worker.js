7f9cb13d5660c6efd8c4581affeabe39456ddc0b
amfelix <amfelix@gmail.com>
2026-07-27 08:39:32 +0100

    Adiciona sessões 61-70 do Nível Intermédio
    Must vs. have to vs. should, gerúndio como sujeito, phrasal verbs
    (look forward to/put up with/run into), idiomas de comunicação (get
    to the point/beat around the bush), vocabulário crime/lei e artes/
    entretenimento, situação 'Biblioteca/Museu', leitura de biografia,
    escrita de narrativa curta, e sessão integradora 'Relatar um Pequeno
    Incidente'.

--- service-worker.js
+++ service-worker.js
@@ -37,6 +37,7 @@ const FICHEIROS_PRECACHE = [
   './content/intermedio/sessoes-31-40.json',
   './content/intermedio/sessoes-41-50.json',
   './content/intermedio/sessoes-51-60.json',
+  './content/intermedio/sessoes-61-70.json',
   'https://unpkg.com/dexie@4.0.8/dist/dexie.min.js'
 ];
 
